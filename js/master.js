b9b2f98cb46862ecf401338e63b646a305716ccd
Yvonne H <yvonnehelland@outlook.com>
2020-02-28 14:20:29 +0100

    Questions answered

---
+++ js/master.js
@@ -0,0 +1,25 @@
+$("#question1").addClass("answer");
+
+$(".question2").addClass("answer");
+
+$(".question3 > span").addClass("answer");
+
+$(".question4 div").addClass("answer");
+
+$(".question5 > div > div > div").addClass("answer");
+
+$(".question6 > div > div > p, .question6 > div > p").addClass("answer");
+
+$(".question7 > div > div > p, .question7 > div > a").addClass("answer");
+
+$(".question8 > ul > li:first-child").addClass("answer");
+
+$(".question9 > ul > li:last-child").addClass("answer");
+
+$(".question10 > ul > li:last-child > a:last-child").addClass("answer");
+
+$(".question11 > ul > li > [href]").addClass("answer");
+
+$(".question12 > ul > li > [href^='http:']").addClass("answer");
+
+$(".question13 > ul > li > a:not([href])").addClass("answer");
